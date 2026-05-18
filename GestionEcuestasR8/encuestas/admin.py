ced47ef4ffa3ba66f5a4b21fe2d152cee48bd57d
Jsagarcrespo <j.sagarcrespo@gmail.com>
2026-05-18 21:41:48 +0200

    feature: Modelado de encuestas y preguntas
    - A los modelos se les mete con sus realciones ForeignKey y configuracion inicial para preguntas de opcion multiple

--- GestionEcuestasR8/encuestas/admin.py
+++ GestionEcuestasR8/encuestas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Encuesta, Pregunta, Opcion
+
 # Register your models here.
+
+admin.site.register(Encuesta)
+admin.site.register(Pregunta)
+admin.site.register(Opcion)
--- GestionEcuestasR8/encuestas/models.py
+++ GestionEcuestasR8/encuestas/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Encuesta(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+    
+    
+class Pregunta(models.Model):
+    encuesta = models.ForeignKey(
+        Encuesta, 
+        on_delete=models.CASCADE, 
+        related_name='preguntas'
+    )
+
+    texto = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.texto
+    
+
+class Opcion(models.Model):
+    pregunta = models.ForeignKey(
+        Pregunta,
+        on_delete=models.CASCADE, 
+        related_name='opciones'
+        )
+    
+    texto = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.texto
