8bda0d673a26687cb2867f19c2e3b3512b072aa0
Jsagarcrespo <j.sagarcrespo@gmail.com>
2026-05-21 21:46:17 +0200

    feature: Añadir editar encuesta reutilizando formularios
    - Reutilizacion de EncuestaForm para edicion mediante instance=encuesta
    - Conectamos la vista detalle con la edición mediante enlaces dinamicos

--- GestionEcuestasR8/encuestas/urls.py
+++ GestionEcuestasR8/encuestas/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('lista/', views.lista_encuestas, name='lista_encuestas'),
     path('crear/', views.crear_encuesta, name='crear_encuesta'),
     path('<int:encuesta_id>/', views.detalle_encuesta, name='detalle_encuesta'),
+    path('<int:encuesta_id>/editar/',views.editar_encuesta,name='editar_encuesta'),
 ]
