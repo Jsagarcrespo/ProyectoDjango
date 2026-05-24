1c424b70eb20db9d42befb7b178486cc8361c858
Jsagarcrespo <j.sagarcrespo@gmail.com>
2026-05-24 18:38:54 +0200

    test: Conexion con la BBDD de Neon
    - instalamos dependencias: pip install psycopg2-binary dj-database-url python-decouple
    - actualizamos: pip freeze > requirements.txt
    - creamos entorno virtual, ejemplo en .env.example
    - modificamos settings.py y comprobar que todo este bien: python manage.py check
    - crear tablas en Neon: python manage.py migrate
    - cargar los datos en Neon: python manage.py loaddata datos_inicialesR8.json

--- GestionEcuestasR8/GestionEcuestasR8/settings.py
+++ GestionEcuestasR8/GestionEcuestasR8/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
+
+from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m*7*pw*brz)r+dqmt6jclaib!dy)q-&5er6#2!h$=$r&zewp^d'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,10 +80,9 @@ WSGI_APPLICATION = 'GestionEcuestasR8.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
